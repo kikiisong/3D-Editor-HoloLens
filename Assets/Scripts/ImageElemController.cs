e4711916b358829f916e6d9d407e46195ae8aac6
kikiisong <55461623+kikiisong@users.noreply.github.com>
2022-04-13 15:41:45 -0400

    import image panel

--- Assets/Scripts/ImageElemController.cs
+++ Assets/Scripts/ImageElemController.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using System.Threading.Tasks;
 using System.IO;
 
 #if UNITY_EDITOR 
     using UnityEditor;
-#elif WINDOWS_UWP // We only have these namespaces if on an UWP device
-    using Windows.Storage;
 #endif
 
 public class ImageElemController : MonoBehaviour
@@ -26,8 +23,9 @@ public class ImageElemController : MonoBehaviour
         
     }
 
-    public async Task createImg()
+    public void createImg()
     {
+        GameObject imgPanel = Instantiate(imgPrefab, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 1), Quaternion.identity);
 #if UNITY_EDITOR
         string path = EditorUtility.OpenFilePanel("Overwrite with jpg", "", "jpg");
         if (path.Length != 0)
@@ -36,37 +34,10 @@ public class ImageElemController : MonoBehaviour
             var tex = new Texture2D(2, 2);
             tex.LoadImage(fileContent);
 
-            GameObject imgPanel = Instantiate(imgPrefab, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 1), Quaternion.identity);
-            MeshRenderer imgRenderer = (MeshRenderer)(imgPanel.transform.GetChild(1).gameObject.GetComponent("MeshRenderer"));
-            imgRenderer.material.mainTexture=tex;
-        }
-
-#elif WINDOWS_UWP
-        var picker = new Windows.Storage.Pickers.FileOpenPicker();
-        picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-        picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-        picker.FileTypeFilter.Add(".jpg");
-        picker.FileTypeFilter.Add(".jpeg");
-        picker.FileTypeFilter.Add(".png");
-
-        Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
-        if (file != null)
-        {
-             // Application now has read/write access to the picked file
-             Windows.Storage.Streams.IBuffer buffer = await Windows.Storage.FileIO.ReadBufferAsync(file);                      
-             Windows.Storage.Streams.DataReader dataReader = Windows.Storage.Streams.DataReader.FromBuffer(buffer);
-             var fileContent = new byte[buffer.Length];     
-             dataReader.ReadBytes(fileContent);
-
-            var tex = new Texture2D(2, 2);
-            tex.LoadImage(fileContent);
-
-            GameObject imgPanel = Instantiate(imgPrefab, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 1), Quaternion.identity);
+            //GameObject imgPanel = Instantiate(imgPrefab, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 1), Quaternion.identity);
             MeshRenderer imgRenderer = (MeshRenderer)(imgPanel.transform.GetChild(1).gameObject.GetComponent("MeshRenderer"));
             imgRenderer.material.mainTexture=tex;
         }
 #endif
-
-        //Instantiate(imgPrefab, new Vector3(-0.3661f, 0, 0.2f), Quaternion.identity);
     }
 }
