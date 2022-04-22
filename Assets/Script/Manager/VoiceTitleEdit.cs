5399a3e88ec0ce4993b67a5ec4334eeb747d0797
kikiisong <55461623+kikiisong@users.noreply.github.com>
2022-04-22 17:35:05 -0400

    assign button event at runtime

--- Assets/Script/Manager/VoiceTitleEdit.cs
+++ Assets/Script/Manager/VoiceTitleEdit.cs
@@ -1,3 +1,4 @@
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,10 +8,14 @@ public class VoiceTitleEdit : MonoBehaviour
 {
     public TextMeshPro textContent;
     public TextMeshPro textTitle;
+    public ButtonConfigHelper buttonTitle;
+    public ButtonConfigHelper buttonContent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonTitle.OnClick.AddListener(startEditingTitle);
+        buttonContent.OnClick.AddListener(startEditingContent);
     }
 
     // Update is called once per frame
