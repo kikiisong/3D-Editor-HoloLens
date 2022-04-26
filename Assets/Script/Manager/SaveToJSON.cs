03638e6ade955e1a5d6852e091d0592ed137566b
kikiisong <55461623+kikiisong@users.noreply.github.com>
2022-04-25 20:02:17 -0400

    find all text Panels

---
+++ Assets/Script/Manager/SaveToJSON.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveToJSON : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void saveToFile()
+    {
+        //for each text panel
+        VoiceTitleEdit[] textPanelsInScene = GameObject.FindObjectsOfType<VoiceTitleEdit>();
+        Debug.Log(textPanelsInScene.Length);
+        
+    }
+}
+
+[System.Serializable]
+public class TextPanel
+{
+    public string title;
+    public string content;
+    public SerializedTransform addonSerializedTransform;
+}
+
+// Found on Unity Forum (TO REF)
+[System.Serializable]
+public class SerializedTransform
+{
+    public float[] _position = new float[3];
+    public float[] _rotation = new float[4];
+    public float[] _scale = new float[3];
+
+
+    public SerializedTransform(Transform transform, bool worldSpace = false)
+    {
+        _position[0] = transform.localPosition.x;
+        _position[1] = transform.localPosition.y;
+        _position[2] = transform.localPosition.z;
+
+        _rotation[0] = transform.localRotation.w;
+        _rotation[1] = transform.localRotation.x;
+        _rotation[2] = transform.localRotation.y;
+        _rotation[3] = transform.localRotation.z;
+
+        _scale[0] = transform.localScale.x;
+        _scale[1] = transform.localScale.y;
+        _scale[2] = transform.localScale.z;
+
+    }
+}
+
