94cd98d96b5f5395fcabb5877829746c7b05d716
kikiisong <55461623+kikiisong@users.noreply.github.com>
2022-04-28 15:41:40 -0400

    load text pane;

--- Assets/Script/Manager/LoadFromJson.cs
+++ Assets/Script/Manager/LoadFromJson.cs
@@ -1,9 +1,11 @@
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadFromJson : MonoBehaviour
 {
+    public GameObject textPanelPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,23 @@ public class LoadFromJson : MonoBehaviour
         string filePath = "Assets/Resources/data.json";
         string UILayoutData = System.IO.File.ReadAllText(filePath);
         UILayout currentUILayout = JsonUtility.FromJson<UILayout>(UILayoutData);
-        Debug.Log(currentUILayout.allTextPanels.ToString());
+        //Debug.Log(currentUILayout.allTextPanels.ToString());
+        foreach (TextPanel currTextPanel in currentUILayout.allTextPanels)
+        {
+            //Debug.Log(JsonUtility.ToJson(currTextPanel));
+            InstantiateTextPanel(currTextPanel);
+        }
+    }
+
+    public void InstantiateTextPanel(TextPanel m_textPanel)
+    {
+        GameObject currPanel = Instantiate(textPanelPrefab, new Vector3(m_textPanel.m_addonSerializedTransform._position[0], m_textPanel.m_addonSerializedTransform._position[1], m_textPanel.m_addonSerializedTransform._position[2]), 
+            new Quaternion(m_textPanel.m_addonSerializedTransform._rotation[1], m_textPanel.m_addonSerializedTransform._rotation[2], m_textPanel.m_addonSerializedTransform._rotation[3],
+            m_textPanel.m_addonSerializedTransform._rotation[0])); 
+        currPanel.transform.localScale = new Vector3(m_textPanel.m_addonSerializedTransform._scale[0], m_textPanel.m_addonSerializedTransform._scale[1], m_textPanel.m_addonSerializedTransform._scale[2]);
+        DialogShell texts = (DialogShell)(currPanel.GetComponent("DialogShell"));
+        texts.TitleText.text = m_textPanel.m_Title;
+        texts.DescriptionText.text = m_textPanel.m_Content;
     }
 }
 
