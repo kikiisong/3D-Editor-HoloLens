1bdf4403568946e12ff1560bd0a38208ab2dcc90
kikiisong <55461623+kikiisong@users.noreply.github.com>
2022-05-02 21:17:55 -0400

    img panel loaded w/o img

--- Assets/Script/Manager/SaveToJSON.cs
+++ Assets/Script/Manager/SaveToJSON.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
 
 public class SaveToJSON : MonoBehaviour
 {
@@ -23,18 +24,30 @@ public class SaveToJSON : MonoBehaviour
     }
 
     public void saveToFile()
-    {      
-        GameObject[] allTextPanel = GameObject.FindGameObjectsWithTag("TextPanel");
+    {
         UILayout currentUILayout = new UILayout();
+
+        GameObject[] allTextPanel = GameObject.FindGameObjectsWithTag("TextPanel");
         foreach (GameObject currTextElem in allTextPanel)
         {
-            
                 VoiceTitleEdit currTextPanel = (VoiceTitleEdit)(currTextElem.GetComponent("VoiceTitleEdit"));
                 TextPanel textPanelToSave = new TextPanel(currTextPanel.textTitle.text, currTextPanel.textContent.text, currTextElem.transform);
                 string textPanelData = JsonUtility.ToJson(textPanelToSave);
                 Debug.Log(textPanelData);
                 currentUILayout.allTextPanels.Add(textPanelToSave);
-                //System.IO.File.WriteAllText("Assets/Resources/data.json", textPanelData);
+        }
+
+        GameObject[] allImgPanel = GameObject.FindGameObjectsWithTag("ImagePanel");
+        foreach (GameObject currImgElem in allImgPanel)
+        {
+            MeshRenderer imgRenderer = (MeshRenderer)(currImgElem.transform.GetChild(1).gameObject.GetComponent("MeshRenderer"));
+            Texture texToSave = imgRenderer.material.mainTexture;
+            TextMeshPro imgTitle = (TextMeshPro)(currImgElem.transform.GetChild(0).GetChild(0).GetComponent("TextMeshPro"));
+            ImgPanel imgPanelToSave = new ImgPanel(imgTitle.text, texToSave, currImgElem.transform);
+
+            string imgPanelData = JsonUtility.ToJson(imgPanelToSave);
+            Debug.Log(imgPanelData);
+            currentUILayout.allImgPanels.Add(imgPanelToSave);
         }
 
         string UILayoutData = JsonUtility.ToJson(currentUILayout);
@@ -96,9 +109,24 @@ public class SaveToJSON : MonoBehaviour
 public class UILayout
 {
     public List<TextPanel> allTextPanels = new List<TextPanel>();
+    public List<ImgPanel> allImgPanels = new List<ImgPanel>();
 
 }
 
+[System.Serializable]
+public class ImgPanel
+{
+    public string m_Title;
+    public Texture m_img;
+    public SerializedTransform m_addonSerializedTransform;
+
+    public ImgPanel(string a_Title, Texture a_img, Transform a_transform)
+    {
+        m_Title = a_Title;
+        m_img = a_img;
+        m_addonSerializedTransform = new SerializedTransform(a_transform);
+    }
+}
 
 [System.Serializable]
 public class TextPanel
