c611fe1d9a9f7f3615542f52218bc13089acc78e
kikiisong <55461623+kikiisong@users.noreply.github.com>
2022-05-02 22:40:36 -0400

    replace image working in editor

--- Assets/Script/ImportedImg.cs
+++ Assets/Script/ImportedImg.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class ImportedImg : MonoBehaviour
@@ -15,5 +17,23 @@ public class ImportedImg : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void useNewImg()
+    {
+#if UNITY_EDITOR
+        string path = EditorUtility.OpenFilePanel("Overwrite with jpg", "", "jpg");
+        if (path.Length != 0)
+        {
+            var fileContent = File.ReadAllBytes(path);
+            var tex = new Texture2D(2, 2);
+            tex.LoadImage(fileContent);
+
+            MeshRenderer imgRenderer = (MeshRenderer)(this.gameObject.transform.GetChild(1).gameObject.GetComponent("MeshRenderer"));
+            imgRenderer.material.mainTexture = tex;
+
+            m_imagePath = path;
+        }
+#endif
     }
 }
